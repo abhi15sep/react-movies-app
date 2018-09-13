3e74df12ec13aa7d4638996616ada7544127fdba
pvlatin <patricio.v@live.cl>
2018-09-13 16:11:47 -0300

    [Experiment] add text

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
 const Movie = ({ movie }) => 
   <div>
     <h3>{movie.title}</h3>
-    <h3>DEVELOP</h3>
+    <h3>experiment</h3>
     <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>
   </div>
   
